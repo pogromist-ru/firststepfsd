03eedfd96e6a3d59be03d1bf04d8b4f9294bbcb7
pogromist-ru <1@thisemaildoesntexist.com>
2022-03-31 10:13:08 +0500

    add importAll function

--- src/index.js
+++ src/index.js
@@ -3,4 +3,61 @@
 import './styles/index.scss';
 import $ from 'jquery';
 
+function importAll(resolve) {
+    resolve.keys().forEach(resolve);
+}
+importAll(
+    require.context(
+        "./components",
+        true,
+        /\.(css|scss|jpg|png|svg|ico|xml|mp4|js)$/
+    )
+);
+
+
+
 $('.block').html('jQuery is working')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Описание require и require.context - https://webpack.js.org/guides/dependency-management/
+// Импорт scss поштучно
+/*
+require("./components/header/header.scss");  // или: import './components/header/header.scss';
+*/
+
+
+// Импорт js в компонентах, в js уже прописываем import './header.scss';
+/*
+function importAll(r) {
+    return r.keys().forEach(r);
+}
+importAll(require.context('./components', true, /\.js$/));
+*/
+
+
+// Импорт сразу js, scss и всего прочего
+/*
+function importAll(resolve) {
+    resolve.keys().forEach(resolve);
+}
+importAll(
+    require.context(
+        "./components",
+        true,
+        /\.(css|scss|jpg|png|svg|ico|xml|mp4|js)$/
+    )
+);
+*/
